e91c9adc5592f58849fba0ad3358a584cfbf593a
geraldy1st <geraldy.leondas@gmail.com>
2022-10-01 13:55:20 +0200

    header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import Cta from "./Cta";
+import MyPhoto from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
+
 import "./Header.styles.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-      <p>This is the header</p>
-    </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Geraldy LEONDAS</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <Cta />
+        <HeaderSocials />
+        <div className="me">
+          <img src={MyPhoto} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
   );
 };
 
