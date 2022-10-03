8764df29727688b3a3dc7871a1c6f0d0eb5fbbb7
geraldy1st <geraldy.leondas@gmail.com>
2022-10-04 01:05:37 +0200

    services component maj

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,96 @@
 import React from "react";
 import "./Services.styles.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
   return (
     <section id="services">
-      <h1>Services</h1>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem dolor sit amet.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem ipsum dolor sit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem sit amet.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of UI/UX */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem dolor sit amet.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem dolor sit amet.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem ipsum dolor sit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem sit amet.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem dolor sit amet.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <BiCheck className="service__lis-icon" />
+              <p>Lorem sit amet.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
     </section>
   );
 };
