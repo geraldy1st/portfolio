05de8fe8b507afb47791c353545e500f8c7ae19e
geraldy1st <geraldy.leondas@gmail.com>
2022-10-10 15:31:44 +0200

    name changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="container header__container" id="home">
         <h5>Hello I'm</h5>
         <h1>Geraldy LEONDAS</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">Frontend Developer</h5>
         <Cta />
         <HeaderSocials />
         <div className="me">
