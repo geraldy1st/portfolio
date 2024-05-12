898eaa378967922f19639b8deaf19ec0f6aaba9d
geraldy1st <geraldy.leondas@gmail.com>
2024-05-12 15:50:02 +0100

    header fix

--- src/components/portfolio/DataPortfolio.jsx
+++ src/components/portfolio/DataPortfolio.jsx
@@ -1,18 +1,18 @@
-import Img1 from "../../assets/portfolio5.png";
+// import Img1 from "../../assets/portfolio5.png";
 import Img2 from "../../assets/portfolio2.jpg";
-import Img3 from "../../assets/portfolio3.jpg";
-import Img4 from "../../assets/portfolio4.jpg";
+// import Img3 from "../../assets/portfolio3.jpg";
+// import Img4 from "../../assets/portfolio4.jpg";
 // import Img5 from "../../assets/portfolio1.jpg";
 // import Img6 from "../../assets/portfolio6.jpg";
 
 const DataPortfolio = [
-  {
-    id: 1,
-    image: Img1,
-    title: "Drump Touch App",
-    github: "https://github.com/geraldy1st/music_mmh",
-    demo: "https://geraldy1st.github.io/music_mmh/",
-  },
+  //   {
+  //     id: 1,
+  //     image: Img1,
+  //     title: "Drump Touch App",
+  //     github: "https://github.com/geraldy1st/music_mmh",
+  //     demo: "https://geraldy1st.github.io/music_mmh/",
+  //   },
 
   {
     id: 2,
@@ -21,20 +21,20 @@ const DataPortfolio = [
     github: "https://github.com/geraldy1st/github-finder",
     demo: "https://githubfinder365.netlify.com/",
   },
-  {
-    id: 3,
-    image: Img3,
-    title: "Crypto Currency",
-    github: "https://github.com/geraldy1st/cryptoGame",
-    demo: "https://rawgit.com/geraldy1st/cryptoGame/master/index.html",
-  },
-  {
-    id: 4,
-    image: Img4,
-    title: "Portfolio  -v",
-    github: "https://github.com/geraldy1st",
-    demo: "https://geraldy1st.github.io/portfolio/",
-  },
+  //   {
+  //     id: 3,
+  //     image: Img3,
+  //     title: "Crypto Currency",
+  //     github: "https://github.com/geraldy1st/cryptoGame",
+  //     demo: "https://rawgit.com/geraldy1st/cryptoGame/master/index.html",
+  //   },
+  //   {
+  //     id: 4,
+  //     image: Img4,
+  //     title: "Portfolio  -v",
+  //     github: "https://github.com/geraldy1st",
+  //     demo: "https://geraldy1st.github.io/portfolio/",
+  //   },
   //   {
   //     id: 5,
   //     image: Img5,
